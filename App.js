8183d14f97a652893a4b0c0e91fc228239d3c778
Gabriel Gircenko <gabriel.gircenko@gmail.com>
2018-12-17 02:31:52 +0100

    TextInput, logging and state changing

--- App.js
+++ App.js
@@ -7,29 +7,52 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, TextInput, Text, View} from 'react-native';
 
 export default class App extends Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      username: "defUsername",
+      password: ""
+    }
+    this.handleUsernameText=this.handleUsernameText.bind(this)
+    this.handlePasswordText=this.handlePasswordText.bind(this)
+  }
+
+  handleUsernameText(newText) {
+    console.log("Username: " + newText)
+    this.setState({
+      username: newText
+    })
+  }
+
+  handlePasswordText(newText) {
+    console.log(`Password: ${newText}`)
+    this.setState({
+      password: newText
+    })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.half1}>
-          <Text style={styles.text}>This is area #1</Text>
-        </View>
-        <View style={styles.half2}>
-          <View style={[styles.half2x, styles.half21]}>
-            <Text style={styles.text}>This is area #2</Text>
-          </View>
-          <View style={[styles.half2x, styles.half22]}>
-            <Text style={styles.text}>This is area #3</Text>
-          </View>
-        </View>
+        <Text>Username</Text>
+        <TextInput 
+          underlineColorAndroid="red"
+          defaultValue={this.state.username}
+          onChangeText={this.handleUsernameText}
+        />
+        <Text>Password</Text>
+        <TextInput 
+          secureTextEntry={true}
+          underlineColorAndroid="red"
+          defaultValue={this.state.password}
+          onChangeText={this.handlePasswordText}
+        />
+        <Text>This is the username: {this.state.username}</Text>
+        <Text>This is the password: {this.state.password}</Text>
       </View>
     );
   }
@@ -37,32 +60,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  half1: {
     flex: 1,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  half2: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  half2x: {
-    flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  half21: {
-    backgroundColor: 'green'
-  },
-  half22: {
-    backgroundColor: 'blue'
-  },
-  text: {
-    color: 'white',
-    fontSize: 30
+    padding: 16,
+    justifyContent: 'center'
   }
 });
